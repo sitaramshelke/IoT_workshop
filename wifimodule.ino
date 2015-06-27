d64dbeb3b22eac9b9715f0aeff0e00bdfced482a
Sitaram Shelke <sitaramshelke@gmail.com>
2015-06-27 12:23:50 +0530

    Update wifimodule.ino

--- wifimodule.ino
+++ wifimodule.ino
@@ -1,61 +1,65 @@
-
 #include <SoftwareSerial.h>
-#include <ESP8266wifi.h>
-SoftwareSerial myTAH(10,11);  //RX,TX
+SoftwareSerial myTAH(10,11); //RX,TX
 void setup() {
-  Serial.begin(9600);
-  while(!Serial)
-  {
-      
-  }
-  myTAH.begin(9600);
-  delay(1000);
-  String ok = "AT";
-  myTAH.println(ok);
-  delay(1000);
-  String cwmode = "AT+CWMODE=3";
-  myTAH.println(cwmode);
-  delay(1000);
-  String rst = "AT+RST";
-  myTAH.println(rst);
-  delay(1000);
-  String mux = "AT+CIPMUX=1";
-  myTAH.println(mux);
-  delay(1000);
-  String conn = "AT+CWJAP=\"Ashwini\",\"12345678\"";
-  myTAH.println(conn);
-  delay(1000);
-  String ip = "AT+CIFSR";
-  myTAH.println(ip);
-  delay(1000);
-  String strt = "AT+CIPSTART=4,\"TCP\",\"";
-  strt += "54.86.132.254";
-  strt += "\",80";
-  l1:
-  myTAH.println(strt);
-  if(myTAH.find("Error"))
-  {
-    Serial.println("AT+CIPSTART error");
-    myTAH.println("AT+CIPMUX=1");
-    goto l1;
-  }
-  //String sent="AT+CIPSEND=4,40";
-  //myTAH.println(strt);
-  Serial.write("Success!!!");
-  
-  
-  //String 
+Serial.begin(9600);
+while(!Serial)
+{
+}
+myTAH.begin(9600);
+delay(1000);
+String ok = "AT";
+myTAH.println(ok);
+delay(1000);
+String cwmode = "AT+CWMODE=3";
+myTAH.println(cwmode);
+delay(1000);
+String rst = "AT+RST";
+myTAH.println(rst);
+delay(1000);
+String mux = "AT+CIPMUX=1";
+myTAH.println(mux);
+delay(1000);
+String conn = "AT+CWJAP=\"Ashwini\",\"12345678\"";
+myTAH.println(conn);
+delay(1000);
+String ip = "AT+CIFSR";
+myTAH.println(ip);
+delay(1000);
+String strt = "AT+CIPSTART=4,\"TCP\",\"";
+strt += "54.86.132.254";
+strt += "\",80";
+l1:
+myTAH.println(strt);
+if(myTAH.find("Error"))
+{
+Serial.println("AT+CIPSTART error");
+myTAH.println("AT+CIPMUX=1");
+goto l1;
+}
+Serial.println("Success");
+String sent="AT+CIPSEND=4,40";
+myTAH.println(strt);
+if(myTAH.available())
+{
+Serial.write(myTAH.read());
+}
+String get = "GET /output/KJ1pL2jgjdfEOWp0XVWE.json/r/n";
+myTAH.println(get);
+if(myTAH.available())
+{
+Serial.write(myTAH.read());
+}
+//Serial.println("Recieved");
+//String
 }
 void loop() {
-
-  if(myTAH.available())
-  {
-   Serial.write(myTAH.read()); 
-   
-  }
-  if(Serial.available())
-  {
-    myTAH.write(Serial.read()); 
-  }    
-  //aread();
+  
+if(myTAH.available())
+{
+Serial.write(myTAH.read());
+}
+if(Serial.available())
+{
+myTAH.write(Serial.read());
+}
 }
