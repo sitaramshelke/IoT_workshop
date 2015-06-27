6b4e67ff29a7823f5b36609a52d650bf742b409c
Sitaram Shelke <sitaramshelke@gmail.com>
2015-06-27 11:38:52 +0530

    Update wifimodule.ino

--- wifimodule.ino
+++ wifimodule.ino
@@ -1,3 +1,4 @@
+
 #include <SoftwareSerial.h>
 #include <ESP8266wifi.h>
 SoftwareSerial myTAH(10,11);  //RX,TX
@@ -8,51 +9,27 @@ void setup() {
       
   }
   myTAH.begin(9600);
-  delay(3000);
+  delay(1000);
   String ok = "AT";
   myTAH.println(ok);
-  while(myTAH.available())
-  {
-    Serial.write(myTAH.read());
-  }
-  delay(3000);
+  delay(1000);
   String cwmode = "AT+CWMODE=3";
   myTAH.println(cwmode);
-  while(myTAH.available())
-  {
-    Serial.write(myTAH.read());
-  }
-  delay(3000);
-    String rst = "AT+RST";
+  delay(1000);
+  String rst = "AT+RST";
   myTAH.println(rst);
-  while(myTAH.available())
-  {
-    Serial.write(myTAH.read());
-  }
-  delay(3000);
+  delay(1000);
   String mux = "AT+CIPMUX=1";
   myTAH.println(mux);
-  while(myTAH.available())
-  {
-    Serial.write(myTAH.read());
-  }
-  delay(3000);
-  String conn = "AT+CWJAP=\"Ashwini\",\"12345678\"";
+  delay(1000);
+  String conn = "AT+CWJAP=\"Anuj\'s iPhone\",\"panipuri\"";
   myTAH.println(conn);
-  while(myTAH.available())
-  {
-    Serial.write(myTAH.read());
-  }
-  delay(3000);
+  delay(1000);
   String ip = "AT+CIFSR";
   myTAH.println(ip);
-  while(myTAH.available())
-  {
-    Serial.write(myTAH.read());
-  }
-  delay(3000);
+  delay(1000);
   String strt = "AT+CIPSTART=4,\"TCP\",\"";
-  strt += "http://data.sparkfun.com/output/KJ1pL2jgjdfEOWp0XVWE";
+  strt += "54.86.132.254";
   strt += "\",80";
   l1:
   myTAH.println(strt);
@@ -62,8 +39,10 @@ void setup() {
     myTAH.println("AT+CIPMUX=1");
     goto l1;
   }
-  String send_str = "AT+CIPSEND=4,18";
-  myTAH.println(send_str);
+  //String sent="AT+CIPSEND=4,40";
+  //myTAH.println(strt);
+  Serial.write("Success!!!");
+  
   
   //String 
 }
@@ -72,6 +51,7 @@ void loop() {
   if(myTAH.available())
   {
    Serial.write(myTAH.read()); 
+   
   }
   if(Serial.available())
   {
